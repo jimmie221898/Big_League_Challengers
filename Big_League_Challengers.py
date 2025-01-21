79f8c1d476f53e5df8751c857f560b3a8554c430
James Byrne <jimmie221898@gmail.com>
2025-01-20 22:25:49 -0500

    Added game clock and start menu image.

--- Big_League_Challengers.py
+++ Big_League_Challengers.py
@@ -2,16 +2,22 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((1800,900))
-
+screen = pygame.display.set_mode((950,800))
 pygame.display.set_caption("Big League Challengers")
 icon = pygame.image.load("Assets/Game_icon.png")
 pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
+Game_Menu = pygame.image.load('Assets/Game_menu.png').convert()
 
 running = True
 while running:
+
+    screen.blit(Game_Menu,(-60,-200))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    pygame.display.flip()
 
 pygame.quit()
